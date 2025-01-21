54a47931357361408c7e6b3b5c757dcf7a35faa7
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-01-21 19:24:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let name1 = prompt("ism kiriting")
+let b = prompt("harf kiriting")
+if(name1.includes(b)){
+    alert(`${name1} ismini ichida ${b} mavjud`)
+}
+else{
+    alert(` ${name1} ismini ichida ${b} harfi mavjud emas` )
+}
